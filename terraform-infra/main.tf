4615e12d6f3c681b1eb8e3a28eeaff695225bb9e
AbdurRehman924 <77286697+AbdurRehman924@users.noreply.github.com>
2026-01-05 16:14:11 +0500

    refactor: create infrastructure-only terraform module
    - Move infrastructure code to terraform-infra/ directory
    - Include DOKS cluster, Redis database, and container registry
    - Remove microservices deployment from Terraform
    - Add terraform.tfvars.example template

---
+++ terraform-infra/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+# DigitalOcean Project
+data "digitalocean_project" "hipster_shop" {
+  name = var.project_name
+}
+
+# DOKS Cluster
+resource "digitalocean_kubernetes_cluster" "hipster_shop" {
+  name    = "hipster-shop"
+  region  = var.region
+  version = "1.34.1-do.2"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-4gb"
+    node_count = 3
+  }
+}
+
+# Container Registry
+resource "digitalocean_container_registry" "hipster_shop" {
+  name                   = "hipster-shop"
+  subscription_tier_slug = "basic"
+}
+
+# Redis Database
+resource "digitalocean_database_cluster" "redis" {
+  name       = "hipster-shop-redis"
+  engine     = "redis"
+  version    = "7"
+  size       = "db-s-1vcpu-1gb"
+  region     = var.region
+  node_count = 1
+}
+
+# Assign resources to project
+resource "digitalocean_project_resources" "hipster_shop" {
+  project = data.digitalocean_project.hipster_shop.id
+  resources = [
+    digitalocean_kubernetes_cluster.hipster_shop.urn,
+    digitalocean_database_cluster.redis.urn
+  ]
+}
